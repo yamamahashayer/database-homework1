c1d48fc137c612bd9ca5df38583ae9c307ee0dbc
yamamah ashayer <159881188+yamamahashayer@users.noreply.github.com>
2024-02-13 06:12:39 -0800

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import java.io.File;
+import  java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ * @author hp
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args)throws  IOException
+    {
+       
+        
+        NewJFrame Frame1=new NewJFrame();
+        Frame1.setVisible(true);
+        
+      
+       
+    }
+}
